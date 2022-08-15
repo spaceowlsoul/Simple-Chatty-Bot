7ddb2d2ea040ad106fda8d38e4c65dde2b5ddb09
Yulia <97407462+spaceowlsoul@users.noreply.github.com>
2022-08-15 17:02:19 +0300

    Update Simple_Chatty_Bot.js

--- Simple_Chatty_Bot.js
+++ Simple_Chatty_Bot.js
@@ -38,7 +38,7 @@ function test() {
 	Why do we use methods?
 	1. To repeat a statement multiple times.
 	2. To decompose a program into several small subroutines.
-    3. To determine the execution time of a program.
+	3. To determine the execution time of a program.
 	4. To interrupt the execution of a program.`);
 
 	let rightAnswer = 2;
